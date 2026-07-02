d78f407325dccc2f6903cf9ccf2b9a994e869c90
J.A CHEEMA <cheemakela@gmail.com>
2026-07-02 18:43:53 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+
+# Pulseaudio ka error fix kiya (--start ki jagah -D)
+pulseaudio -D --system --disallow-exit --disable-shm
 service xrdp start
 
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-# --- NGROK KO START KARNE KA SYSTEM ---
-if [ ! -z "$NGROK_AUTHTOKEN" ]; then
-    ngrok config add-authtoken $NGROK_AUTHTOKEN
-    ngrok tcp 3389 --log=stdout > /var/log/ngrok.log &
-fi
+# --- BORE TUNNEL START KARNA ---
+echo "Starting Bore Tunnel for RDP (Port 3389)..."
+# Yeh command port 3389 ko internet par open kar degi
+bore local 3389 --to bore.pub > /var/log/bore.log 2>&1 &
+
+# Thora intezar karein aur link print karein
+sleep 3
+cat /var/log/bore.log
 
 tail -f /var/log/xrdp-sesman.log
