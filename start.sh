8728477752f43401adfdac9515172d4e305f13ee
J.A CHEEMA <cheemakela@gmail.com>
2026-07-02 18:31:45 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,10 @@ service xrdp start
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
+# --- NGROK KO START KARNE KA SYSTEM ---
+if [ ! -z "$NGROK_AUTHTOKEN" ]; then
+    ngrok config add-authtoken $NGROK_AUTHTOKEN
+    ngrok tcp 3389 --log=stdout > /var/log/ngrok.log &
+fi
+
 tail -f /var/log/xrdp-sesman.log
